4c5e9c67a8948cf445a53e6780c6d4a0b593a618
Sam Kornstein <skornstein@gmail.com>
2014-03-02 15:51:07 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beerington::Application.config.secret_key_base = 'a3c75aa2ac9acfc042541a907e1f45e5a4e77f9685be88b545b3f1dc31019ad2c0f96796fe94f4a9a36e4bee5bc13f4ea11c1beb22cbb77905de8acb2c789389'
